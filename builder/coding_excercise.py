545b75cb545c00513529d5133142bce4e5b9784b
Venus <valievavm@gmail.com>
2020-10-17 00:20:16 +0200

    Update builder coding exercise to print raw code

--- builder/coding_excercise.py
+++ builder/coding_excercise.py
@@ -8,22 +8,34 @@
 # def __init__(self):
 # self.name = ""
 # self.age = 0
-import inspect
 
 
 class CodeBuilder:
+    indent = ' '
+
     def __init__(self, root_name):
         self.obj = eval(root_name)()  # instantiate class
 
+    def get_obj_attrs(self):
+        obj_attrs = {k: v for k, v in self.obj.__dict__.items() if not k.startswith('__')}
+        return obj_attrs
+
     def add_field(self, field, value):
-        try:
-            setattr(self.obj, field, value)  # set attr
-        except AttributeError:
-            print('No such attribute.')
-        return self
+        if field in self.get_obj_attrs().keys():
+            try:
+                setattr(self.obj, field, value)  # set attr
+            except AttributeError:
+                print('No such attribute.')
+            return self
 
     def __str__(self):
-        return str(self.obj)
+        lines = []
+        lines.append(f"class {self.obj.__class__.__name__}:")
+        lines.append(f"{self.indent * 2} def __init__(self):")
+        for field, value in self.get_obj_attrs().items():
+            lines.append(f'{self.indent * 4}{field} = {value}')
+
+        return str('\n'.join(lines))
 
 
 class Person:
@@ -37,4 +49,3 @@ class Person:
 
 cb = CodeBuilder('Person').add_field('name', '""').add_field('age', '0')
 print(cb)
-# TODO -print code
